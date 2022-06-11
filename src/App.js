eeb9f374ef8e13a3670745ade3b3058072d23b04
LinaRente <linarente@gmail.com>
2022-06-11 12:05:30 +0200

    test donnees api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,46 @@
-import React from "react";
+import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
-
 function App() {
-  return (
-    <div className="App">
-      <h1>League of Legends</h1>
-    </div>
-  );
+    const [searchText, setSearchText] = useState("");
+    const [gameList, setgameList] = useState([]);
+
+    function getPlayerGames() {
+        axios.get("http://localhost:4000/past5Games", { params: { username: searchText } })
+            .then(response => {
+                setgameList(response.data)
+            }).catch((error) => {
+                console.log(error)
+            })
+    }
+    return (
+        <div className='App'>
+            <h1>League Of Leagends</h1>
+            <p>Players last 5 games</p>
+            <input type="text" className='searchbar' name='search' onChange={e => setSearchText(e.target.value)} placeholder='choose your username'/>
+            <button className='buttonsearchbar' onClick={getPlayerGames}>Search</button>
+            {gameList.length !== 0 ?
+                <>
+                    {gameList.map((gameData, index) =>
+                        <>
+                            <h2>Game {index + 1}</h2>
+                            <div className='card'>
+                                {gameData.info.participants.map((data, participantIndex) =>
+                                    <p>PLAYER {participantIndex + 1}: {data.summonerName}, KDA: {data.kills} / {data.deaths} / {data.assists}</p>
+                                )}
+                            </div>
+                        </>
+                    )
+                    }
+                </>
+                :
+                <>
+                    <p>No Player </p>
+                </>
+            }
+        </div>
+    );
 }
 
 export default App;
